2a7b7b4ad4002d4dd9f10a65fc8435d346baeed4
dpointeck <daniel.pointecker@gmail.com>
2019-10-20 08:43:22 +0200

    bug: get the right scoreres per match

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ import {
   getTableContent,
   writeTableFile,
   getMatchesContent,
+  getMatchesContent2,
   writeMatchesFile,
   getScorer
-} from './lib/scraper';
-import fs from 'fs';
+} from "./lib/scraper";
+import fs from "fs";
 
 async function go() {
   const url =
-    'https://nachrichten.at/sport/fussball/unterhaus/2019_2020/1_klasse/1nw/kopfing';
+    "https://nachrichten.at/sport/fussball/unterhaus/2019_2020/1_klasse/1nw/kopfing";
 
   const table = await getTableContent(await getHtml(url));
 
   await writeTableFile(table);
 
-  const matches = await getMatchesContent(await getHtml(url));
+  const matches = await getMatchesContent2();
 
   await writeMatchesFile(matches);
 
-  console.log('scrape complete\n');
+  console.log("scrape complete\n");
 }
 
 go();
