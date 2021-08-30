a14aabc657d20c070c2ee3ca4e91599e67664630
Daniel Pointecker <daniel.pointecker@gmail.com>
2021-08-30 19:43:40 +0200

    tweaks

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ import {
   getMatchesContent,
   getMatchesContent2,
   writeMatchesFile,
-  getScorer
+  getScorer,
+  sleep
 } from "./lib/scraper";
 import fs from "fs";
 
 async function go() {
   const url =
-    "https://nachrichten.at/sport/fussball/unterhaus/2019_2020/1_klasse/1nw/kopfing";
+    "https://nachrichten.at/sport/fussball/unterhaus/2020_2021/1_klasse/1nw/kopfing";
 
   const table = await getTableContent(await getHtml(url));
 
@@ -22,6 +23,10 @@ async function go() {
   await writeMatchesFile(matches);
 
   console.log("scrape complete\n");
+
+  await sleep(2000);
+
+  process.exit()
 }
 
 go();
