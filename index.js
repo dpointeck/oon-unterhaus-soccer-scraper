3d4e472fadbea253a2354be0537ebd5ca178e66d
dpointeck <daniel.pointecker@gmail.com>
2024-02-20 20:29:45 +0100

    New idea

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ import fs from "fs";
 import { URL } from "./lib/scraper";
 
 async function go() {
-  // const table = await getTableContent(await getHtml(URL));
+  const html = await getHtml(URL);
+  const table = await getTableContent(await getHtml(URL));
 
-  // await writeTableFile(table);
+  await writeTableFile(table);
 
-  const matches = await getMatchesContent2(URL);
-
+  const matches = await getMatchesContent(html);
   const kopfingMatches = matches.filter((item) => {
     return ((item.home === "Kopfing") || (item.guest === "Kopfing"));
   })
